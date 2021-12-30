0c9026867d5cc48067513d8a24ceefaffd7d34d2
Andre Antunes Vieira <andreantunesv@gmail.com>
2021-12-30 12:40:19 -0300

    start mariene project

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+const MinhaPagina = () => (
+	<>
+		<h1>Mariene</h1>
+		<p>kdashk</p>
+		<img height="200" src="https://static1.purepeople.com.br/articles/0/30/07/00/@/3403342-beyonce-iconica-7-provas-que-a-cantora-624x600-1.jpg"/>
+	</>
+)
+
+
+export default MinhaPagina
